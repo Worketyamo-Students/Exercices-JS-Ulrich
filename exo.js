444c460d5b240d68dd68adc6514d5d3c6b32ab11
UlrichDevp <ulrichsoh72@gmail.com>
2025-02-26 18:21:58 +0100

    feat:Les 7 exos

---
+++ exo.js
@@ -0,0 +1,62 @@
+// console.log("Exercice1")
+
+// let nom = prompt("Entrer votre Nom")
+// const age = prompt("Entrer votre age")
+// console.log("Bienvenue Mr/Mm", nom, "agé de", age, "ans")
+
+
+
+// console.log("Exercice2")
+
+// function total(c, n){
+//     c = prompt("entrer le cout total par participants")
+//     n = prompt ("entrer le nombre total de participants")
+//    return console.log("le cout total est: ", c * n, "Fcfa")
+// }
+// total()
+
+
+// console.log("Exercice3")
+
+// function reduction(a){
+//     if (a < 18){
+//         console.log("Vous etes eligible à une reduction")
+//     } else {
+//         console.log("Vous n'etes pas eligible à une reduction")
+//     }
+// }
+// reduction(28)
+
+
+
+// console.log("Exercice4")
+
+// let x = Number(prompt("entrez une note"))
+// let y = Number(prompt("entrez une deuxieme note"))
+// let z = Number(prompt("entrez une troisieme note"))
+// let somme = x + y + z
+// console.log("La moyenne est: ", somme/3)
+
+
+
+// console.log("Exercice5")
+
+// let name = "Gojo"
+// let year = 28
+// let inscrit = true
+// console.log(typeof name)
+// console.log(typeof year)
+// console.log(typeof inscrit)
+
+console.log("Exercice6")
+let x = prompt("un nom")
+var tableau = ["Paul", "Jean", "Maurice", x]
+var table2 = tableau.push("Stephane")
+console.log(tableau)
+
+// console.log("Exercice7")
+
+// let nouveau = prompt("Entrez le nom d'un nouvel inscrit")
+// let alerte = nouveau + " a été inscrit(e)"
+// alert(alerte)
+// console.log("Bienvenue a vous Mr/Mm: ", nouveau)
---
+++ index.js
@@ -0,0 +1,87 @@
+// //perimetre du carré
+// let coté= 8;
+// let périmètre= coté * 4;
+// console.log("le périmètre de ce carré est: ", périmètre, "cm")
+// //surface du triangle
+// let base= 8;
+// let hauteur1= 10;
+// let aire1= (base * hauteur1) / 2;
+// console.log("l'aire de ce triangle est: ", aire1, "cm²")
+// //volume de la pyramide
+// let longueur= 10;
+// let largeur= 7;
+// let hauteur2= 10;
+// let aire2= longueur * largeur;
+// let volume= (aire2 * hauteur2) / 3;
+// console.log("le volume de cette pyramide est: ", volume, "cm³")
+
+//EXERCICE
+// let x = 15
+// let y = 30
+// let sup = x > y
+// let inf = x < y
+// let eg = x == y
+// let diff = x != y
+// console.log("Comparaison de la supériorité de x et y: ", sup)
+// console.log("Comparaison de l'infériorité de x et y: ", inf)
+// console.log("Comparaison de l'egalité entre x et y: ", eg)
+// console.log("Comparaison de la difference entre x et y: ", diff)
+// console.log("x est superieur et inferieur à y? : ", sup && inf)
+// console.log("x est superieur ou inferieur à y? : ", sup || inf)
+// console.log("x est inferieur ou egal à y? : ", inf || eg)
+// console.log("x est superieur ou egal à y? : ", sup || eg)
+// console.log("x est superieur/inferieur et egal à y? : ", (sup || inf) && eg)
+// console.log("x est inférieur et different de y? : ", inf && diff)
+// console.log("x est supérieur ou different de y? : ", sup || diff)
+// console.log("x est different et egal à y? : ", diff && eg)
+// console.log("x est different ou egal à y? : ", diff || eg)
+
+// LES FONCTIONS EN JS
+
+// function F1(L, l){
+//     let p = L + l
+//     return [2*p, p]
+// }
+// console.log("Le perimetre et le demi perimetre sont: ", F1(20, 10))
+
+// function F2(L,l){
+//     q = L*L + l*l
+//     let H = Math.sqrt(q)
+//     return H
+// }
+// console.log("L'hypothénuse est: ", F2(10,5))
+
+
+// let objet = {
+//     nom : "Paul",
+
+//     prenom : "Biya",
+
+//     age : 40
+// }
+// console.log(objet)
+// console.log(typeof objet)
+
+//STRUCTURES DE CONTROLE
+
+// let age = prompt("Veuillez entrer un age")
+// function verification(){
+//     return console.log("Votre age est: ", age, "ans")
+// }
+// verification()
+// function sup(){
+//     return console.log("l'utilisateur est majeur")
+// }
+// function inf(){
+//     return console.log("l'utilisateur est mineur")
+// }
+// if (age > 18){
+//     sup()
+// } else{
+//     inf()
+// } 
+
+
+//String
+
+
